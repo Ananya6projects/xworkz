4bf78ea0966c890130e0483afaae25d3f8c4b512
Ananya V <ananyaav20@gmail.com>
2025-04-03 18:35:31 +0530

    Delete

--- inherit/inheritance/internal/festival/Diwali.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.inheritence.internal.festival;
-
-public class Diwali extends Festival {
-    public Diwali() {
-        super();
-        System.out.println("Running non-arg constructor of Diwali");
-    }
-}
--- inherit/inheritance/internal/fruit/Apple.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.inheritence.internal.fruit;
-
-public class Apple extends Fruit {
-    public Apple() {
-        super();
-        System.out.println("Running non-arg constructor of Apple");
-    }
-}
