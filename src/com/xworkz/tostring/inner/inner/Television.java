37ed832a8fc445886d47c1b03d9fbf4f974dd586
Ananya6projects <ananyaav20@gmail.com>
2025-04-15 18:46:08 +0530

    Tostring.hashcode.equals

---
+++ src/com/xworkz/tostring/inner/inner/Television.java
@@ -0,0 +1,38 @@
+package com.xworkz.tostring.inner.inner;
+
+public class Television {
+    private String propertyOne;
+    private int propertyTwo;
+    private boolean propertyThree;
+
+    public Television(String propertyOne, int propertyTwo, boolean propertyThree) {
+        this.propertyOne = propertyOne;
+        this.propertyTwo = propertyTwo;
+        this.propertyThree = propertyThree;
+    }
+
+    @Override
+    public String toString() {
+        return "Television [propertyOne=" + propertyOne + ", propertyTwo=" + propertyTwo + ", propertyThree=" + propertyThree + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return 123;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Television television = (Television) obj;
+        return propertyTwo == television.propertyTwo &&
+                propertyThree == television.propertyThree &&
+                propertyOne.equals(television.propertyOne);
+    }
+
+}
